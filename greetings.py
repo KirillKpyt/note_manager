285be4a8d8b63ea94ce2e7949fa73e321088dce3
KirillKpyt <long.may@mail.ru>
2025-01-10 13:32:23 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = 'Ivan'
+title = 'Case1'
+content = 'Complete Case1'
+status = 'Active'
+created_date ="10-01-2025"
+issue_date = "11-01-2025"
+
+print('Имя пользователя', username)
+print('Заголовок заметки',title)
+print('Описание заметки',content)
+print('Статус заметки',status)
+print('Дата создания заметки в формате "день-месяц-год"',created_date)
+print('Дата истечения заметки (дедлайн) в формате "день-месяц-год"', issue_date)
+
